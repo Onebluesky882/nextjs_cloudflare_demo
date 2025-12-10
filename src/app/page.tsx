b1de8fabdf9e1fe693d878604986655f1a0500bc
Onebluesky882 <onebluesky882@outlook.com>
2025-12-10 14:33:48 +0700

    fix : middleware

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default async function Home() {
     <div className="flex flex-col items-center   border border-amber-50 py-50 rounded-2xl ">
       <h1>Hello world</h1>
       <p>build with cloudflare</p>
-      <p>hellow from {headerList.get("X-User-Country")}</p>
+      <p>Welcome from {headerList.get("X-User-Country")}</p>
     </div>
   );
 }
