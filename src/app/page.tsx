abd6c9142b691993e4a72de19af41d00aa220841
Onebluesky882 <onebluesky882@outlook.com>
2025-12-10 13:21:45 +0700

    update homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
-    <div>
-		  <h1>Hello world</h1>
-		  <p>build with cloudflare</p>
+    <div className="flex justify-center border border-amber-50">
+      <h1>Hello world</h1>
+      <p>build with cloudflare</p>
     </div>
   );
 }
